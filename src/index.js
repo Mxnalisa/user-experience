538c76421cae9f9d1f9e86693d332c6d6bdb3c73
Mxnalisa <amomalungs@gmail.com>
2025-06-23 12:38:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "d7852e3faob94308902a4f5e758t6a40";
   let context =
-    "You are a poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions";
+    "You are a poem expert and love to write short poems. Your mission is to generate a 4 line poem stanza. Make sure to follow the user instructions";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
