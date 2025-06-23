dbe5bdcee5f6592892f119c2a69a721c2b39a854
Mxnalisa <amomalungs@gmail.com>
2025-06-23 10:40:32 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "d7852e3faob94308902a4f5e758t6a40";
+  let context =
+    "You are a poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
